d30f43b5652cdc94d836b3a7871df20449c530c7
Kurt Colonia <krcolonia@gmail.com>
2025-07-17 19:36:52 +0800

    Added a couple of personal notes for compiling and running C++ programs

--- GeekForGeeks-Certificate/program-one.cpp
+++ GeekForGeeks-Certificate/program-one.cpp
@@ -6,4 +6,15 @@ int main()
 {
 	cout << "Hello World!";
 	return 0;
+
+	// single line comment
+	/* multi-line comment */
+
+	/* 
+	? Command to compile and run the program
+	
+	g++ [filename].cpp -o [exe_filename]
+	./[exe_filename]
+
+	*/
 }
